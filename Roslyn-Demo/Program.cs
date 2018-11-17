24091ea02c8f5b226eabe473097d8797e17ad816
taylus <therealbjohnson@gmail.com>
2018-11-17 06:53:57 -0600

    Add project files.

---
+++ Roslyn-Demo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Roslyn_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
